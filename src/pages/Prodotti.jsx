0b2ce07ed191d3cef47a64596c08bd0cd3f8bfa3
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-06 19:01:36 +0200

    aggiunto useEffect con chiamata axios per recuperare prodotti dall’API

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Prodotti = () => {
+  const [prodotti, setProdotti] = useState([]);
+
+  //   AXIOS
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProdotti(response.data);
+      })
+      .catch((error) => {
+        console.error("errore recupero dati prodotti", error);
+      });
+  }, []);
+
+  //   LISTA PRODOTTI
   return (
     <>
       <div>Questa è la pagina prodotti</div>
